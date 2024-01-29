c5d081e197f26bbf76331a9a099cccc2514c8088
Jolan <jolan.brizard@epitech.eu>
2024-01-29 16:12:12 +0100

    refactor(script): rename script update

--- rename.sh
+++ rename.sh
@@ -6,18 +6,13 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-# Store the replacement string
 replacement_string=$1
 
-# Find files containing 'test' in the current directory and its subdirectories, excluding /node_modules
-files=$(grep -rl 'test' --exclude-dir=node_modules --exclude=bun.lockb .)
+files=$(grep -rl 'fastbun' --exclude-dir=node_modules --exclude-dir=dist --exclude-dir=.git --exclude=bun.lockb .)
 
-echo $files
-
-# Loop through each file and replace 'test' with the provided string
 for file in $files; do
     sed -i "" "s/fastbun/$replacement_string/g" $file
-    echo "Replaced 'test' with '$replacement_string' in $file"
+    echo "Replaced 'fastbun' with '$replacement_string' in $file"
 done
 
 echo "Replacement complete."
